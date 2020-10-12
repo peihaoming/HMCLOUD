8b2955d9480cf917b1b4a8bfff28dfd5a8fc325b
haomingp <peihaoming@reyun.com>
2020-10-12 11:42:52 +0800

    公用服务

---
+++ cloud-api-commonm/src/test/java/com/hm/cloudapicommonm/CloudApiCommonmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.cloudapicommonm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiCommonmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

10e531c911ef91ff0e29b22c15ce43a57c842f02
haomingp <peihaoming@reyun.com>
2020-10-13 15:07:30 +0800

    gateway

---
+++ cloud-gateway-gateway9092/src/main/java/com/hm/cloudgatewaygateway9092/CloudGatewayGateway9092Application.java
@@ -0,0 +1,13 @@
+package com.hm.cloudgatewaygateway9092;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayGateway9092Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayGateway9092Application.class, args);
+    }
+
+}

0eca4fb68afa5f95647e31933918fbc9567a28fc
haomingp <peihaoming@reyun.com>
2020-10-15 19:00:46 +0800

    配置中心服务测试

---
+++ cloud-config-center/src/test/java/com/hm/cloudconfigcenter/CloudConfigCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.cloudconfigcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

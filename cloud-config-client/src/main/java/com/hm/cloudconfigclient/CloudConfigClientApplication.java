f8e0ff8f1ee9904b44a21b51c99e760c23427544
haomingp <peihaoming@reyun.com>
2020-10-19 16:11:13 +0800

    加入消息总线支持配置文件动态刷新

--- cloud-config-client/src/main/java/com/hm/cloudconfigclient/CloudConfigClientApplication.java
+++ cloud-config-client/src/main/java/com/hm/cloudconfigclient/CloudConfigClientApplication.java
@@ -1,12 +1,15 @@
 package com.hm.cloudconfigclient;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableRabbit
 public class CloudConfigClientApplication {
 
     public static void main(String[] args) {
